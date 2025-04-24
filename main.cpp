5c8023e4008acd1731828b01d13efca2c674ea35
YuniXIII <maximsandu999@gmail.com>
2025-04-24 18:23:53 +0000

    added functions

--- main.cpp
+++ main.cpp
@@ -1,39 +1,38 @@
 #include "mbed.h"
 #include "TextLCD_CC.h"
 
+TextLCD lcd(D0, D1, D2, D3, D4, D5, TextLCD::LCD16x2);
 
 InterruptIn exit_button(D6,PullUp);    //console button
-
 InterruptIn left_up(D7,PullUp);     //left controller buttons
 InterruptIn left_down(D8,PullUp);
 InterruptIn left_enter(D9,PullUp);
-
 InterruptIn right_up(D10,PullUp);   //right controller buttons
 InterruptIn right_down(D11,PullUp);
 InterruptIn right_enter(D12,PullUp);
 
 Thread button_function;
+Semaphore button_sem(0,1);      //semaphore to use with buttons
 
-Semaphore button_sem(0,1);      //semaphores to use with buttons
 
-void sem_release();
-void token_release();
-
+void arrow_change_func();
 void up_release();
 void down_release();
 void enter_release();
-void exit_release();
+void exit_release(); 
 
 bool up = false;
 bool down = false;
 bool enter = false;
 bool exit_key = false;
 
+void main_menu_func();
+
 // main() runs in its own thread in the OS
 int main()
 {
-    button_function.start(token_release);
-
+    button_function.start(arrow_change_func);
+    
     left_up.fall(up_release);
     right_up.fall(up_release);
 
@@ -45,8 +44,9 @@ int main()
 
     exit_button.fall(exit_release);
 
-
-    while (true){};
+    main_menu_func();
+    
+    while (true) {};
 }
 
 void up_release(){
@@ -66,6 +66,35 @@ void exit_release(){
     exit_key = true;
 }
 
-void token_release(){
+void arrow_change_func(){
+    button_sem.acquire();
+    if (up == true){
+        lcd.cls(); //clears the lcd screen
+        lcd.locate(0,0); //set cursor on row number 1 
+        lcd.printf("> Flying Ace"); //displays the select option for "Flying Ace"
+        lcd.locate(0,1);//set cursor on row 2
+        lcd.printf("  Combat Ace"); //displays the select option for "Combat Ace"
+        up = false;
+    }
 
+    if (down == true){
+        lcd.cls(); //clears the lcd screen
+        lcd.locate(0,0); //set cursor on row number 1 
+        lcd.printf("  Flying Ace"); //displays the select option for "Flying Ace"
+        lcd.locate(0,1);//set cursor on row 2
+        lcd.printf("> Combat Ace"); //displays the select option for "Combat Ace"
+        down = false;
+    }
+}
+
+void main_menu_func(){
+    lcd.cls(); //clears the lcd screen
+    lcd.locate(0,0); //set cursor on row number 1 
+    lcd.printf("   Gaming Ace\n"); //displays the message Hello World on the LCD
+    thread_sleep_for(2000);
+
+    lcd.locate(0,0); //set cursor on row number 1 
+    lcd.printf("> Flying Ace"); //displays the select option for "Flying Ace"
+    lcd.locate(0,1);//set cursor on row 2
+    lcd.printf("  Combat Ace"); //displays the select option for "Combat Ace"
 }
