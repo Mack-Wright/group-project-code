7f66dbd01a7f4e8368d6544edd4a12282157bdc7
YuniXIII <maximsandu999@gmail.com>
2025-04-24 14:24:41 +0000

    initial code

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "mbed.h"
+#include "TextLCD_CC.h"
+
+
+InterruptIn exit_button(D6,PullUp);    //console button
+
+InterruptIn left_up(D7,PullUp);     //left controller buttons
+InterruptIn left_down(D8,PullUp);
+InterruptIn left_enter(D9,PullUp);
+
+InterruptIn right_up(D10,PullUp);   //right controller buttons
+InterruptIn right_down(D11,PullUp);
+InterruptIn right_enter(D12,PullUp);
+
+Thread button_function;
+
+Semaphore button_sem(0,1);      //semaphores to use with buttons
+
+void sem_release();
+void token_release();
+
+void up_release();
+void down_release();
+void enter_release();
+void exit_release();
+
+// main() runs in its own thread in the OS
+int main()
+{
+    button_function.start(token_release);
+
+    left_up.fall(up_release);
+    right_up.fall(up_release);
+
+    left_down.fall(down_release);
+    right_down.fall(down_release);
+
+    left_enter.fall(enter_release);
+    right_enter.fall(enter_release);
+
+    exit_button.fall(exit_release);
+
+
+    while (true){};
+}
+
+void up_release(){
+    button_sem.release();
+    
+}
+
+void token_release(){
+
+}
