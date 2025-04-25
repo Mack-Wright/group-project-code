72fe30bbec4452cd2496fc71559c099acf7c2a71
YuniXIII <maximsandu999@gmail.com>
2025-04-25 11:37:27 +0000

    buggy up down

--- main.cpp
+++ main.cpp
@@ -7,96 +7,114 @@ InterruptIn exit_button(D6,PullUp);    //console button
 
 InterruptIn left_up(D7,PullUp);     //left controller buttons
 InterruptIn left_down(D8,PullUp);
-InterruptIn left_enter(D9,PullUp);
 
 InterruptIn right_up(D10,PullUp);   //right controller buttons
 InterruptIn right_down(D11,PullUp);
-InterruptIn right_enter(D12,PullUp);
 
-Thread button_function;
+
+Thread buttons;
+
 Semaphore button_sem(0,1);      //semaphore to use with buttons
 
 
 void arrow_change_func();
+void poll_buttons();
+
+
 void up_release();
 void down_release();
-void enter_release();
 void exit_release(); 
 
 bool up = false;
 bool down = false;
-bool enter = false;
+bool wait = false;
 bool exit_key = false;
 
 void main_menu_func();
 
+
 // main() runs in its own thread in the OS
 int main()
 {
-    button_function.start(arrow_change_func);
-    
-    left_up.fall(up_release);
-    right_up.fall(up_release);
-
-    left_down.fall(down_release);
-    right_down.fall(down_release);
-
-    left_enter.fall(enter_release);
-    right_enter.fall(enter_release);
-
-    exit_button.fall(exit_release);
-
+    buttons.start(poll_buttons);
+  
     main_menu_func();
+    arrow_change_func();
     
-    while (true) {};
+    while (true) {
+        arrow_change_func();
+    };
 }
 
 void up_release(){
     button_sem.release();
     up = true;
+    
 }
 void down_release(){
     button_sem.release();
     down = true;
+   
 }
-void enter_release(){
-    button_sem.release();
-    enter = true;
-}
+
+
 void exit_release(){
     button_sem.release();
     exit_key = true;
 }
 
 void arrow_change_func(){
-    button_sem.acquire();
-    if (up == true){
-        lcd.cls(); //clears the lcd screen
-        lcd.locate(0,0); //set cursor on row number 1 
-        lcd.printf("> Flying Ace"); //displays the select option for "Flying Ace"
-        lcd.locate(0,1);//set cursor on row 2
-        lcd.printf("  Combat Ace"); //displays the select option for "Combat Ace"
-        up = false;
-    }
+    
+    while (wait == false){
+        button_sem.acquire();
+        wait = true;
 
-    if (down == true){
+        if (up == true){
         lcd.cls(); //clears the lcd screen
         lcd.locate(0,0); //set cursor on row number 1 
-        lcd.printf("  Flying Ace"); //displays the select option for "Flying Ace"
+        lcd.printf(">Singleplayer"); //displays the select option for "Flying Ace"
         lcd.locate(0,1);//set cursor on row 2
-        lcd.printf("> Combat Ace"); //displays the select option for "Combat Ace"
+        lcd.printf(" Multiplayer"); //displays the select option for "Combat Ace"
+        
+        
+        }
+
+        else{
+        if (down == true){
+            lcd.cls(); //clears the lcd screen
+            lcd.locate(0,0); //set cursor on row number 1 
+            lcd.printf(" Singleplayer"); //displays the select option for "Flying Ace"
+            lcd.locate(0,1);//set cursor on row 2
+            lcd.printf(">Multiplayer"); //displays the select option for "Combat Ace"
+            
+            }
+    
+        }
+        wait = false;
+        up = false;
         down = false;
     }
+    
 }
 
 void main_menu_func(){
     lcd.cls(); //clears the lcd screen
     lcd.locate(0,0); //set cursor on row number 1 
-    lcd.printf("   Gaming Ace\n"); //displays the message Hello World on the LCD
+    lcd.printf("   Flying Ace\n"); //displays the message Hello World on the LCD
     thread_sleep_for(2000);
 
     lcd.locate(0,0); //set cursor on row number 1 
-    lcd.printf("> Flying Ace"); //displays the select option for "Flying Ace"
+    lcd.printf(">Singleplayer"); //displays the select option for "Flying Ace"
     lcd.locate(0,1);//set cursor on row 2
-    lcd.printf("  Combat Ace"); //displays the select option for "Combat Ace"
+    lcd.printf(" Multiplayer"); //displays the select option for "Combat Ace"
+}
+
+void poll_buttons(){
+    left_up.fall(up_release);
+    left_down.fall(down_release);
+
+    exit_button.fall(exit_release);
+
+    right_up.fall(up_release);
+    right_down.fall(down_release);
 }
